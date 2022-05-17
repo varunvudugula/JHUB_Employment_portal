193a13c4e706ef6e25ecf5c73daf1bcbc52e2da8
varunvudugula <42871058+varunvudugula@users.noreply.github.com>
2022-05-17 11:18:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import pyttsx3
+import speech_recognition as sr
+speaker=pyttsx3.init()
+speaker.say('ritish is lover boy')
+speaker.runAndWait()
---
+++ whatsappmsg.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+ 917731978522','love u nani',14,55)
